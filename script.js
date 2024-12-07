23ce652c86d9669a9bf3787d1d7f6cf82c4905d5
Marvin Nox <sinedtex@gmail.com>
2024-12-07 15:50:37 +0200

    head favicon js

---
+++ script.js
@@ -0,0 +1,11 @@
+const modalOpener = document.getElementById("modal-window");
+const openBttn = document.getElementById("open-modal");
+const closeBttn = document.getElementById("close-modal");
+
+openBttn.addEventListener("click", () => {
+  modalOpener.classList.add("is-open");
+});
+
+closeBttn.addEventListener("click", () => {
+  modalOpener.classList.remove("is-open");
+});
